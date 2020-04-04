4b540fcea25d779b75cbe6f98e27f1656f7d29ff
wrs62 <53711809+wrs62@users.noreply.github.com>
2020-04-04 22:50:03 +0200

    first uplaod

---
+++ Utility.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Utility {
+	
+	public static ArrayList<Table> createDailyTables (ArrayList<Table> allTables, ArrayList<String> dailySlots) {	
+		ArrayList<Table> dailyTables = new ArrayList<Table>();
+		for (int i = 0; i < dailySlots.size(); i++) {
+			for (int j = 0; j < allTables.size(); j++) {
+				Table temp = allTables.get(j);
+				temp.setTimeSlot(dailySlots.get(i));
+				dailyTables.add(temp);
+			}
+		}
+		return dailyTables;
+	}
+
+	public void otherTestClass() {
+		;
+	}
+	
+	
+
+}
